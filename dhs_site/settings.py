4bf5340258aed88cb9cd062e9ad13b97ed29930e
Matt Dinsmore <mattdinsmore@hotmail.com>
2019-11-12 21:34:54 -0800

    updated settings.py

--- dhs_site/settings.py
+++ dhs_site/settings.py
@@ -123,4 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#EOF
+
+# Configure Django App for Heroku.
+# import django_heroku
+# django_heroku.settings(locals())
